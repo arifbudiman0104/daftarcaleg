06ec8bdfddc95da327b50a8dde1f36bee030e4d4
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-10-11 21:02:58 +0700

    initial commit

--- resources/views/pendaftaran.blade.php
+++ resources/views/pendaftaran.blade.php
@@ -11,7 +11,8 @@
         </div>
         @endif
 
-        <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm md:shadow-lg mb-10">
+        <section
+            class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm md:shadow-lg mb-0 sm:mb-5 md:mb-10">
 
             <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
                 Form Pendaftaran Caleg
